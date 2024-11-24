ecee2044daa5e31d07e15742a34e8207873bc8b0
Bryan <bryanssv@DESKTOP-5U9SA13>
2024-11-24 17:20:58 -0500

    Sistema Distribuido Bryan Sanchez Y Juan Olarte

--- trabajo_final_sd/pg_slave/init.sql
+++ trabajo_final_sd/pg_slave/init.sql
@@ -1,8 +1,3 @@
--- En el contenedor esclavo (pg_slave/init.sql)
--- Conexión al servidor maestro
-SELECT pg_start_backup('replica_backup', true);
 
--- Conectar el esclavo al maestro
--- Asegúrate de que estos valores coincidan con la configuración del maestro
--- y la dirección del maestro
+SELECT pg_start_backup('replica_backup', true);
 SELECT pg_stop_backup();
